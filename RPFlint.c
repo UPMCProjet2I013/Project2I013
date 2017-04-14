d4b37458d4cb304da6d94c202107152cbc7cd281
WiemZou <wiem.zouali@gmail.com>
2017-04-14 14:49:19 +0200

    Update RPFlint.c

--- RPFlint.c
+++ RPFlint.c
@@ -2,13 +2,14 @@
 #include "fmpz_poly.h"
 
 int main(int argc, char const *argv[]) {
-fmpz_poly_t x, y;
-fmpz_poly_init(x);
-fmpz_poly_set_coeff_ui(x, 3, 6);
-fmpz_poly_set_coeff_ui(x, 2, 5);
-fmpz_poly_set_coeff_ui(x, 1, 4);
-fmpz_poly_set_coeff_ui(x, 0,  3);
-fmpz_poly_print(x); flint_printf("\n");
+  fmpz_poly_t x;
+  fmpz_poly_init(x);
+  fmpz_poly_set_coeff_ui(x, 3, 6);
+  fmpz_poly_set_coeff_ui(x, 2, 5);
+  fmpz_poly_set_coeff_ui(x, 1, 4);
+  fmpz_poly_set_coeff_ui(x, 0,  3);
+  fmpz_poly_print(x); flint_printf("\n");
+
 fmpz_poly_clear(x);
   return 0;
 }
