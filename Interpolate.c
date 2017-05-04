e322d74b8d1d0c6149ef7692ff650f264a046ed0
Salomelette <salomeattiach@hotmail.fr>
2017-05-04 18:50:33 +0200

    Update Interpolate.c

--- Interpolate.c
+++ Interpolate.c
@@ -7,30 +7,28 @@
 //mp_ptr : tab de mp_limb_t
 //mp_srcptr : tab de mp_limb_t const
 
-void interpolatePolynomeQ(int *x, int *y, int deg, int nb_val,fmpq_poly_t *poly){
+void interpolatePolynomeQ(int *x, int *y, int n,fmpq_poly_t *poly){
 	int i;
-	fmpq_poly_t poly2;
 	fmpz *xs,*ys;
-	xs=_fmpz_vec_init(nb_val);
-	ys=_fmpz_vec_init(nb_val);
-	for(i=0;i<nb_val;i++){
+	xs=_fmpz_vec_init(n);
+	ys=_fmpz_vec_init(n);
+	for(i=0;i<n;i++){
 		fmpz_set_si(xs+i,x[i]);
 		fmpz_set_si(ys+i,y[i]);
 	}
-
-	fmpq_poly_interpolate_fmpz_vec(poly2,xs,ys,deg);
-	*poly=poly2;
-
-	//return poly; faut il clear les variables xs et ys à la fin ?
+	printf("avant appel\n" );
+	fmpz_print (xs+3) ;
+	flint_printf ( " \n " ) ;
+	fmpq_poly_interpolate_fmpz_vec(*poly,xs,ys,n-1);
 }
 
 /*Version avec nmod*/
-void interpolatePolmod(int *xs, int *ys, int len, int m){
-	mp_ptr poly=_nmod_vec_init(len);//initialise un vecteur de longueur n
-	_nmod_vec_set(xs, , slong len);
+void interpolatePolmod(int *xs, int *ys, int len, int m,mp_ptr *poly){
+	//mp_ptr poly=_nmod_vec_init(len);initialise un vecteur de longueur n
+	//_nmod_vec_set(*poly,(mp_srcptr)xs,len);
 	nmod_t mod;
-	nmod_init(mod, m);
-	_nmod_poly_interpolate_nmod_vec(poly ,xs, ys , len, mod);
+	nmod_init(&mod, m);
+	_nmod_poly_interpolate_nmod_vec(*poly,(mp_srcptr)xs,(mp_srcptr)ys,len,mod);
 	//return poly;
 }
 
@@ -38,15 +36,16 @@ void interpolatePolmod(int *xs, int *ys, int len, int m){
 
 int main(int argc, char const *argv[]) {
 	fmpq_poly_t x;
-	mp_ptr xm;
-	int n=3;
-	int k=5;
-	int xs[]={1,2,3,4,5};
-	int ys[]={2,4,6,8,10};
-	interpolatePolynomeQ(xs, ys, n, k,&x);
-	interpolatePolmod(xs,ys, k, 5);
+	int n=4;
+	int xs[]={0,1,2,3};
+	int ys[]={2,2,4,8};
+	//mp_ptr poly=_nmod_vec_init(n);
+	printf("main\n" );
+	interpolatePolynomeQ(xs, ys, n,&x);
+	//interpolatePolmod(xs,ys,n,11,&poly);
 	fmpq_poly_print(x); flint_printf("\n");
+	//fmpq_poly_print((fmpq_poly_struct)poly); flint_printf("\n");
 	fmpq_poly_clear(x);
-	_nmod_vec_clear(xm);
+	//nmod_vec_clear(poly);
 	return 0;
 }
